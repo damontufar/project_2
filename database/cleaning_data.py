286e9d94d0050043b5dff70cc5f1028ebb141fb8
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-05-21 18:12:35 -0500

    Web scrapping of countries & join with deaths DF

--- database/cleaning_data.py
+++ database/cleaning_data.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 
 #Extract CSV into DataFrames
 
-read_path = os.path.join('Resources/')
-deaths_df = pd.read_csv(f'{read_path}number-of-deaths-by-risk-factor.csv')
+read_path = os.path.join('Resources')
+deaths_df = pd.read_csv(f'{read_path}/number-of-deaths-by-risk-factor.csv')
 
 #Keep columns useful for analysis
 
@@ -25,7 +25,9 @@ cols_deaths = [ 'Entity',
 
 deathsdf = deaths_df[cols_deaths].copy()
 #Rename columns
-cols_rename = {'Deaths - Secondhand smoke - Sex: Both - Age: All Ages (Number)': 'Secondhand smoke',
+cols_rename = {'Entity': 'Country_Name',
+                'Code': 'Country_Code',
+                'Deaths - Secondhand smoke - Sex: Both - Age: All Ages (Number)': 'Secondhand smoke',
                 'Deaths - Alcohol use - Sex: Both - Age: All Ages (Number)' : 'Alcohol use',
                 'Deaths - Diet low in fruits - Sex: Both - Age: All Ages (Number)': 'Diet low in fruits',
                 'Deaths - Diet low in vegetables - Sex: Both - Age: All Ages (Number)': 'Diet low in vegetables',
@@ -37,29 +39,38 @@ deathsdf = deathsdf.rename(columns = cols_rename)
 
 #Take most recent year of data (2017)
 
-deathsdf = deathsdf[(deathsdf.Year==2017) & (deathsdf.Entity!='Andean Latin America') & (deathsdf.Entity!='World') &
-                    (deathsdf.Entity!='Western Europe') & (deathsdf.Entity!='Western Sub-Saharan Africa')]
-entities = deathsdf['Entity']
+deathsdf = deathsdf[(deathsdf.Year==2017)]
+#Review columns
+print(deathsdf.info())
 
 #Scrape Countries & Codes
 
 url = 'https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes'
-
 table = pd.read_html(url)
 
-#Slice to DataFrame
+#Slice to DataFrame & Drop Level 0
 countries_df = table[0]
-#Select wanted columns
-#Write Path
+countries_df.columns = countries_df.columns.droplevel(0)
+#Select only necessary columns & change name
+countries_df = countries_df[['Country name[5]', 'Alpha-3 code[5]']]
+countries_df.columns = ['Country_Name', 'Country_Code']
+#Review columns
+print(countries_df.info())
 
-#write_path = os.path.join('Output/')
-#entities.to_csv(f'{write_path}entities.csv')
+#Join in order to only keep countries
+
+deathsdf_c = pd.merge(deathsdf, countries_df, on='Country_Code')
+print(deathsdf_c.info())
+
 
 
 #Data Normalization
 #deathsdf_transformed = deathsdf.melt(id_vars = ['Entity', 'Code', 'Year'], var_name = 'risk_factor', value_name='number_deaths')
 #print(deathsdf_transformed.head())
 
+#Write Path
 
+#write_path = os.path.join('Output/')
+#entities.to_csv(f'{write_path}entities.csv')
 
 
