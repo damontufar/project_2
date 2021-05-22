86128260d6f3e8cc26b8b73552c3eeb0f284ffd6
Tania Rosas <angelina.rosas@gmail.com>
2021-05-22 13:20:02 -0500

    YouTube WebScrapping working

--- health.py
+++ health.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import videoRecommendation
@@ -16,20 +15,13 @@ def index():
     Recommendations = mongo.db.Recommendations.find_one()
     return render_template("index.html", Recommendations=Recommendations)
 
-topics = [
-    'smoking+effects',
-    'drinking+effects',
-    'lack+of+exercise+effects'
-    ] # Topics should be pass based on user fit
-
 # Falta llamar a main con una variable
 @app.route("/videoRecommendation")
 def v_recommendation():
-    Recommendations = mongo.db.listings
+    Recommendations = mongo.db.Recommendations
     Recommendations_data = videoRecommendation.videoRecommendation()
     Recommendations.update({}, Recommendations_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- videoRecommendation.py
+++ videoRecommendation.py
@@ -2,47 +2,51 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 topics = [
-    # 'smoking+effects',
-    # 'drinking+effects',
+    'smoking+effects',
+    'drinking+effects',
     'lack+of+exercise+effects'
     ] # Topics should be pass based on user fit
+
+Recommendations = {
+    'v_url': [],
+    'v_title': [],
+    'v_topic': []
+    }
 
 # Falta llamar a main con una variable
 
 def videoRecommendation():
-    driver = webdriver.Chrome()
-    
-    Recommendations = {}
+    driver = webdriver.Chrome() 
 
     # Loop through topics of user
     for topic in topics:
 
         driver.get('https://www.youtube.com/results?search_query={}&sp=CAM%253D'.format(topic))
         content = driver.page_source.encode('utf-8').strip()
+
+        # Scrape page into Soup
         soup = BeautifulSoup(content, 'lxml')
-        titles = soup.findAll('a', id='video-title')
-        views = soup.findAll('span', class_='style-scope ytd-video-meta-block')
+
+        # Find elements
+        # views = soup.findAll('span', class_='style-scope ytd-video-meta-block') # contain index & upload date, so next element of views is view+2
         video_urls = soup.findAll('a',id='video-title')
 
-        
+        # Store data in a dictionary
+        Recommendations['v_url'].append(['https://www.youtube.com' + url.get("href") for url in video_urls])
+        Recommendations['v_title'].append([url.text for url in video_urls])
+        Recommendations['v_topic'].append(topic)
 
-        print('\n\nTopic: {}'.format(topic))
-
-        i=0 # index for view & upload date in views
-        j=0 # index for video_urls
-
-        for title in titles[:10]:
-            print('\n{}\t{}\t{}\thttps://www.youtube.com{}'.format(title.text,views[i].text,views[i+1].text, video_urls[j].get('href')))
-            
-            Recommendations[v_url] = video_urls[j].get('href')
-            Recommendations[v_title] = title.text
-
-            i+=2 # views contain index & upload date, so next set of views got +2 index
-            j+=1 # next url
-            
+        # Print to terminal - Testing.............
+        # print('\n\nTopic: {}'.format(topic))
+        # i=0 # index for view & upload date in views
+        # j=0 # index for video_urls
+        # for title in titles[:10]:
+        #     print('\n{}\t{}\t{}\thttps://www.youtube.com{}'.format(title.text,views[i].text,views[i+1].text, video_urls[j].get('href')))
+        #     i+=2 # views contain index & upload date, so next set of views got +2 index
+        #     j+=1 # next url
+        # print(Recommendations)
 
     return Recommendations
-
 
 # BIBLIOGRAPHY: 
 # Benjamin Carlson. (5 de junio de 2020). "Web Scraping Popular YouTube Videos | Python One Day Builds". Youtube. https://youtu.be/zhkhdD2hkQw
