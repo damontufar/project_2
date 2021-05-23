4bd1b9810d4b612625dac71af9d2a11987c9e360
Rcidm1994 <rodrigoalfredocidmoreno@gmail.com>
2021-05-23 15:43:02 -0500

    commit modifications in htmllandingpage

---
+++ template_2/latestnews.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_health
+
+#create an instance of Flask
+app = Flask(__name__)
+
+#Use Pymongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/health_app")
+
+#delete in production
+mongo.db.collection.drop()
+
+#route to render index.html template using data from mongo
+@app.route("/")
+def news_and_recommendations():
+
+    #find one record of data from mongo database
+    health_data = mongo.db.health_data.find_one()
+    
+    #return template and data
+    return render_template("news_and_recommendations.html", health_data=health_data)
+    
+#route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+
+    health = mongo.db.health_data
+    health_data = scrape_health.scrape()
+    #health.insert_many(health_data)
+    health.update({}, health_data, upsert=True)
+
+    print(health_data)
+
+    return redirect("/", code=302)
+    
+if __name__ == "__main__":
+    app.run(debug=True)     
+
+
+
